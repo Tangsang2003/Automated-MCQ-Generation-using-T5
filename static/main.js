acb75885519a8c2fdc5e756fb7374af37b78ba00
Tangsang2003 <077bei047.tangsang@pcampus.edu.np>
2024-02-25 22:11:27 +0545

    Add Loader

--- static/main.js
+++ static/main.js
@@ -27,4 +27,22 @@ function checkAnswer(selectedOption) {
         const correctAnswerElement = optionsList.nextElementSibling;
         correctAnswerElement.style.display = 'block';
         }
+// loader
 
+function hideLoader(){
+//  $('.page-loader-2').fadeOut('slow');
+  $('.page-loader-2').hide();
+  $('.page-loader').fadeOut('slow');
+}
+
+function hideLoader_two(){
+//  $('.page-loader').fadeOut('slow');
+//  $('.page-loader-2').fadeOut('slow');
+    $('.page-loader').hide();
+    $('.page-loader-2').fadeOut('slow');
+}
+
+ function showLoader() {
+            // Show the loader
+            $('.page-loader-2').fadeIn('slow');
+ }
